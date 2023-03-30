bb0259bf9381349f26e428ac8f423b185811e02f
Rama Fabricio <fabriciofranciscohernandezgome@gmail.com>
2023-03-30 10:33:02 -0600

    Primera version para subir a produccion

---
+++ web/themes/theme_arias/js/theme-arias.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * theme_arias behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.themeArias = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
